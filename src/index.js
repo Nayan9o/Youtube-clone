15de872905447bb71d7bc77912af69e9790fd9aa
Nayan9o <nayanmeshram9o9@gmail.com>
2026-05-24 16:03:29 +0530

    add env config using import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-require('dotenv').config({path:'./env'});
+// require('dotenv').config({path:'./env'});
 import connectDB from "./db/index.js"
+import dotenv from "dotenv";
+
+dotenv.config({
+  path:'./env'
+})
 
 
 connectDB()
