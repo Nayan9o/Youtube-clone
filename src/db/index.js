a6aa232e24002ff90dc70b521e51e774766005dc
Nayan9o <nayanmeshram9o9@gmail.com>
2026-06-05 13:18:40 +0530

    improve database connection logging and structure

--- src/db/index.js
+++ src/db/index.js
@@ -3,11 +3,11 @@ import {DB_NAME} from "../constants.js"
 
 async function connectDB() {
   try{
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    console.log("DB connected successfuly");
+   const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    console.log(`DB connected successfuly ${connectionInstance.Connection.host}`);
     
   }catch(error){
-    console.log("ERROR ",error);
+    console.log("MONGOdb connect failed ",error);
     process.exit(1)
     
   }
