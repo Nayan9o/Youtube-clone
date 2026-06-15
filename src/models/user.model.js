3c36d8d4108307747dba571a7d4c70d674539b3a
Nayan9o <nayanmeshram9o9@gmail.com>
2026-06-15 15:13:33 +0530

    Add password hashing middleware to userSchema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -49,7 +49,11 @@ const userSchema = new Schema({
   timestamps:true
 });
 
-
+userSchema.pre("save",async function(next){
+  if(!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10)
+  next();
+})
 
 
 export const User = mongoose.model("User",userSchema)
