389efe987b58b4ad63d8c1c1c2fb807827003534
Nayan9o <nayanmeshram9o9@gmail.com>
2026-06-15 15:10:50 +0530

    Add bcrypt and jsonwebtoken dependencies; update user model to include JWT and bcrypt imports

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
   username: {
@@ -46,6 +48,8 @@ const userSchema = new Schema({
 },{
   timestamps:true
 });
+
+
 
 
 export const User = mongoose.model("User",userSchema)
