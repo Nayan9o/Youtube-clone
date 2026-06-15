9d0b554d1f774811b0d26423848b2c55d4c4f32d
Nayan9o <nayanmeshram9o9@gmail.com>
2026-06-15 19:35:40 +0530

    Add cloudinary integration for file uploads and fix ApiError stack handling

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from 'cloudinary';
+   cloudinary.config({ 
+        cloud_name: process.env.CLOUDNARY_NAME, 
+        api_key: process.env.CLOUDNARY_API_KEY, 
+        api_secret: process.env.CLOUDNARY_API_SECRET
+    });
+
+const uploadOnCloudnary = async (localFilePath) =>{
+  try {
+    if (!localFilePath) return null
+    //upload file on cloudnary
+    const responce = await cloudinary.uploader.upload(localFilePath,{
+      resource_type:"auto"
+    })
+    console.log("File successfully uploaded at cloudnary",responce.url)//url in console
+    return responce
+  } catch (error) {
+    fs.unlinkSync(localFilePath)//remove temp file on local
+    return null 
+  }
+}
+
+export {uploadOnCloudnary}
