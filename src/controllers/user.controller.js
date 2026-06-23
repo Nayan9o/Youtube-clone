aeaf00e981bc8c3f3229f8d449de47a216a7547b
Nayan9o <nayanmeshram9o9@gmail.com>
2026-06-23 14:23:15 +0530

    Implement validation for user registration fields in registerUser controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,16 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
 
 const registerUser = asyncHandler (async (req,res)=>{
- res.status(200).json({
-  message:"OK"
- })
+//get data form frontend
+const {fullName, username, email, password} = req.body;
+
+//validation 
+if([
+  fullName,username,email,password
+].some((field)=>field?.trim() === "")){
+  throw new ApiError("400","All fields are required!!")
+}
 })
 
 export { registerUser }
