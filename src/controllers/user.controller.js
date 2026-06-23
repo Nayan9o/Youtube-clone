6b82fc9d7b5e15ee1679d6da2fc523ae55c16d51
Nayan9o <nayanmeshram9o9@gmail.com>
2026-06-23 14:32:17 +0530

    Refactor registerUser controller for improved readability and validation logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,26 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {ApiError} from "../utils/ApiError.js"
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
 
-const registerUser = asyncHandler (async (req,res)=>{
-//get data form frontend
-const {fullName, username, email, password} = req.body;
+const registerUser = asyncHandler(async (req, res) => {
+  //get data form frontend
+  const { fullName, username, email, password } = req.body;
 
-//validation 
-if([
-  fullName,username,email,password
-].some((field)=>field?.trim() === "")){
-  throw new ApiError("400","All fields are required!!")
-}
-})
+  //validation
+  if (
+    [fullName, username, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError("400", "All fields are required!!");
+  }
 
-export { registerUser }
+  //if alredy exist
+  const userExist = User.findOne({
+    $or : [{ email }, { username }]
+  })
+
+  if(userExist){
+    throw new ApiError(409,"User with this email or username alredy exist")
+  }
+});
+
+export { registerUser };
