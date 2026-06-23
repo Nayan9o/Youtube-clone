cc4895395efff61ef19cbed33321d899b4ad4478
Nayan9o <nayanmeshram9o9@gmail.com>
2026-06-23 14:44:40 +0530

    Add cloudinary upload utility and enforce avatar requirement in registerUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
+import { uploadOnCloudnary } from "../utils/cloudnary.js"
 
 const registerUser = asyncHandler(async (req, res) => {
   //get data form frontend
@@ -25,6 +26,10 @@ const registerUser = asyncHandler(async (req, res) => {
   //check image or avatar
   const avatarLocalPath = req.files?.avtar[0]?.path;
   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if(!avatarLocalPath){
+    throw new ApiError(400,"Avatar is required")
+  }
 });
 
 export { registerUser };
